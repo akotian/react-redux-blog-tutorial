846d5cabaf2cbbc524d9d293569620b53d7f9fed
Ashley Kotian <ashleykotian@gmail.com>
2017-05-11 08:02:40 -0700

    Dump of tutorial so far

---
+++ src/components/post_index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class PostIndex extends Component {
+
+  render() {
+    return (
+      <div>I am post Index</div>
+    )
+  }
+};
+
+export default PostIndex;
