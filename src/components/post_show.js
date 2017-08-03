e1364d9c43f944f81a44a276a22cc7395f034b51
Ashley Kotian <ashleykotian@gmail.com>
2017-08-02 23:34:15 -0700

    Final Wrapup, delete Post

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import { connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-import {fetchPost} from '../actions';
+import {fetchPost, deletePost} from '../actions';
 
 class PostShow extends Component {
   componentDidMount() {
     const {id} = this.props.match.params
     this.props.fetchPost(id);
+  }
+
+  handleDelete() {
+    const {id} = this.props.match.params
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
+
   }
   render() {
     const {post} = this.props;
@@ -17,6 +26,13 @@ class PostShow extends Component {
 
     return (
       <div>
+        <Link to="/">Home</Link>
+        <button
+          type="button"
+          className="btn btn-danger pull-xs-right"
+          onClick={this.handleDelete.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -30,4 +46,4 @@ function mapStateToProps({posts}, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
 export default function(state={}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      _.omit(state, action.payload);
     case FETCH_POSTS:
       // Convert list to dictionary by id
       return _.mapKeys(action.payload.data, 'id');
